d92bfc67d06a9b059e632dffa09d61bebe886044
Utkarsh Pandey <167892826+UtkarshPandey121@users.noreply.github.com>
2026-01-31 17:32:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (82.99%) - LeetSync

--- 2752-sum-multiples/sum-multiples.cpp
+++ 2752-sum-multiples/sum-multiples.cpp
@@ -4,7 +4,7 @@ public:
         int sum = 0;
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
-                sum += i;
+                sum =sum + i;
             }
         }
         return sum;
